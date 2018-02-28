3b08ce25fc0a936329f5fb440d8254e6e07d38f9
kitekate <cwallez@hotmail.fr>
2018-02-28 15:25:18 +0100

    resolution du pb de hamburger non actif dans le angualr.cli.son

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Article} from './article';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title: string;
+    articles: Array<Article>;
+    editing: boolean;
+
+
+    constructor() {
+        this.editing = false;
+        this.title = 'BlogAngular';
+        this.articles = new Array();
+        this.articles.push(new Article(99, 'Article de test', 'Super description...'));
+    }
+
+    addArticle() {
+        this.editing = true;
+    }
 }
