0c09cb46e09cc525cd68a3c8e173c8403f2066cd
kitekate <cwallez@hotmail.fr>
2018-02-28 12:09:42 +0100

    initialisation du git pour transfert en repository

---
+++ src/app/article.ts
@@ -0,0 +1,22 @@
+/*import de la librairie moment pour gerer les dates*/
+import * as moment from 'moment';
+
+
+export class Article {
+    id: number;
+    title: string;
+    description: string;
+    author: string;
+    date: moment.Moment;
+
+    constructor(id: number, title: string, description?: string, author: string = 'cat') {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.date = moment();
+    }
+
+
+
+}
